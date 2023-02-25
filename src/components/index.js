b35dbb656ad3097a6d55e59e89e1103a6401bca9
Haruna Faruk <harunafaruk2004@gmail.com>
2023-02-25 18:54:52 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import FeedbackCard from "./FeedbackCard";
+import Button from "./Button";
+import Business from "./Business";
+import CTA from "./CTA";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import Clients from "./Clients";
+import Footer from "./Footer";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import CardDeal from "./CardDeal";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  FeedbackCard,
+  Hero,
+  GetStarted,
+  Clients,
+  Stats,
+  Testimonials,
+  Footer,
+  CTA,
+  Button,
+};
